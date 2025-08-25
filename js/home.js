9300961f910cc3f2def3706f5140a552afd7cf32
coderamitdey <amitdey7150@gmail.com>
2025-08-26 00:24:15 +0600

    updated add money section

---
+++ js/home.js
@@ -0,0 +1,17 @@
+document.getElementById("add-money-btn").addEventListener('click',function(event){
+    event.preventDefault()
+    
+    const bank = document.getElementById("bank").value 
+    const accountNumber = document.getElementById("account-number").value
+    const amount = parseInt(document.getElementById("add-amount").value)
+
+    const pin = document.getElementById("add-pin").value
+
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+
+    console.log(availableBalance)
+
+    const totalNewAvailableBalance = amount + availableBalance;
+
+    document.getElementById("available-balance").innerText = totalNewAvailableBalance;
+})
